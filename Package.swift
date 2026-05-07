6ee8975303279ed462ce6881a45e8991b38d9cfd
OS-pedrogustavobilro <pedro.gustavo.bilro@outsystems.com>
2026-05-07 11:50:10 +0100

    fix(ios): lower deployment target to match Capacitor

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 
 let package = Package(
     name: "CapacitorLocalLlm",
-    platforms: [.iOS("18.4.0")],
+    platforms: [.iOS(.v15)],
     products: [
         .library(
             name: "CapacitorLocalLlm",
